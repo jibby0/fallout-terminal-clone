a89ba75c0cd8ea73aaa20b12ce294ba4cb0840b9
Joshua Bicking <jhb2345@rit.edu>
2016-02-14 23:37:03 -0500

    Separated files and cleaned up code is preparation for word reading. Removed crashing OSX build.

---
+++ src/print.c
@@ -0,0 +1,85 @@
+// 
+// File: print.c 
+// print.c handles printing of pre-game strings
+// @author Josh Bicking <josh1147582>
+// // // // // // // // // // // // // // // // // // // // // // // 
+
+#ifdef _WIN32
+#   include <Windows.h>
+#   include <curses.h>
+#   define SLEEP(delay) Sleep(delay/1000)
+// Unix builds require ncurses.h for the Ncurses library.
+// Unix also requires unistd.h for usleep(microseconds).
+// usleep/1000 = Sleep
+#else
+#   include <ncurses.h>
+#   include <unistd.h>
+#   define SLEEP(delay) usleep(delay)
+#endif
+// Thanks to /u/aftli_work for the file consolidation suggestion!
+
+#include <stdlib.h>
+#include <time.h>
+#include "print.h"
+
+void slowPrint(char arr[], int size, int line){
+    for(int i=0; i<size; i++){  
+        /* Print the current character in the current position. */
+        mvprintw(line,i,"%c",arr[i]);
+        /* Move the cursor to the next position */
+        move(line, i+1);
+        refresh();
+        /* If any keyboard input was recieved, go directly to pass(), otherwise continue */
+        if(kbhit()){
+            pass();
+        }
+        SLEEP(20000);
+    }
+    return 0;
+}
+
+void slowType(char arr[], int size, int line){
+    for(int i=0; i<size; i++){  
+        mvprintw(line,i+1,"%c",arr[i]);
+        move(line, i+2);
+        refresh();
+        if(kbhit()){
+            pass();
+        }
+        SLEEP(70000);
+    }
+    return 0;
+}
+
+void passPrint(char arr[], int size, int line){
+    int i;
+    for(i=0; i<size; i++){  
+        mvprintw(line,i,"%c",arr[i]);
+        move(line, i+1);
+        refresh();
+        SLEEP(20000);
+    }
+}
+
+int kbhit(){
+    /* Get the current char. */
+    int ch = getch();
+
+    /* Returns true if a key has been hit. False if it hasn't. */ 
+    if (ch != ERR) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void printChoices(int hex, char arr[], int line, int offset){
+    mvprintw(line,offset,"0x%X", hex);
+    for(int i=0; i<12; i++)
+        mvprintw(line,7+offset+i,"%c",arr[i]);
+    move(line, 20+offset);
+    refresh();
+    SLEEP(30000);
+}
+
+// end
---
+++ src/print.h
@@ -0,0 +1,48 @@
+
+#ifndef PRINT_H
+#define PRINT_H
+
+/// Print characters onto the screen (not player input).
+/// @param arr  string to print
+/// @param size  size of the string
+/// @param line  line on which to print the string
+///
+void slowPrint(char arr[], int size, int line);
+
+
+/// Print characters onto the screen (player input).
+/// @param arr  string to print
+/// @param size  size of the string
+/// @param line  line on which to print the string
+///
+void slowType(char arr[], int size, int line);
+
+
+/// Operates the same way as slowPrint, but cannot be interrupted 
+/// by a key press.
+/// @param arr  string to print
+/// @param size  size of the string
+/// @param line  line on which to print the string
+/// 
+void passPrint(char arr[], int size, int line);
+
+
+/// Checks if a key has been pressed. Used to skip the opening sequence and
+/// jump straight to pass().
+///
+int kbhit();
+
+
+/// Used by pass() to print the left side of the screen. First prints 
+/// a hexidecimal representation of an int, then the section of the 
+/// string containing words and garbage.
+/// @param hex  number to be displayed in hexidecimal
+/// @param arr  string to be printed
+/// @param line  line on which to print the string
+/// @param offset  used to determine which side of the screen to print on
+///
+void printChoices(int hex, char arr[], int line, int offset);
+
+#endif // PRINT_H
+
+// end
